3bc94431b7afbcd8c84917306d3061db2d6988cf
Dmytro Kravchenko <dmitrij.kravchenko.1997@gmail.com>
2023-08-02 12:46:06 +0300

    v. 0.2.2

--- src/js/current.js
+++ src/js/current.js
@@ -3,12 +3,21 @@ const discoverLink = document.getElementById('discover');
 
 const indexHtml1 = '/';
 const indexHtml2 = '/index.html';
+const indexGitHub1 = '/panda-nft-tw';
+const indexGitHub2 = '/panda-nft-tw/index.html';
 const discoverHtml = '/discover.html';
+const discoverGitHub = '/panda-nft-tw/discover.html';
 
-if (location.pathname === indexHtml1 || location.pathname === indexHtml2) {
+const { pathname } = location;
+
+if (pathname === indexHtml1 || pathname === indexHtml2) {
+    homeLink.classList.add('current');
+} else if (pathname === indexGitHub1 || pathname === indexGitHub2) {
     homeLink.classList.add('current');
 };
 
-if (location.pathname === discoverHtml) {
+if (pathname === discoverHtml) {
+    discoverLink.classList.add('current');
+} else if (pathname === discoverGitHub) {
     discoverLink.classList.add('current');
 };
